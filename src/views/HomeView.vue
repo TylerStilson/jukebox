ee69dc403bba2ebbbaaade870f0bc500b903f7dc
Tyler Yablonovsky <tyleryablonovsky@Tylers-MacBook-Pro.local>
2022-11-28 21:53:20 -0700

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="background">
+    <h1>Jukebox</h1>
+    <div id="nav-menu">
+      <h2>All Songs</h2>
+      <h2>Playlists</h2>
+    </div>
+    
   </div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+
   }
-}
 </script>
+
+<style scoped>
+#background{
+  background-image: url('../assets/images/background.svg');
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+}
+h1{
+  font-family: 'baloo-bold';
+  color: white;
+  font-size: 200px;
+  width: 100%;
+  text-align: center;
+  padding-top: 2%;
+}
+h2{
+  font-family: 'baloo';
+  color: #D90504;
+  font-size: 24px;
+}
+#nav-menu{
+  display: flex;
+  justify-content: space-around;
+  margin: 0px 40%;
+}
+</style>
